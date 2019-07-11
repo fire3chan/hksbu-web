05308d71d135e605dc076c7a898c64b88f3d2013
fire chan <cken497@gmail.com>
2019-07-12 00:13:48 +0800

    fire edit header

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Header from './component/Header';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App row">
       <Header></Header>
     </div>
   );
--- src/component/Header.js
+++ src/component/Header.js
@@ -2,7 +2,40 @@ import React from 'react';
 
 class Header extends React.Component {
   render() {
-    return <h1>Hello user!</h1>;
+    return (
+      <div className="col-12">
+        <div className="container">
+          <div className="navbar navbar-expand-lg navbar-light">
+            <div className="site-title">hksbu</div>
+
+            <nav className="nav-bar">
+              <ul className="navbar-nav mr-auto">
+                <li className="nav-item active">
+                  <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">Link</a>
+                </li>
+                <li className="nav-item dropdown">
+                  <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Dropdown
+              </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                </li>
+              </ul>
+            </nav>
+
+            <form className="form-inline my-2 my-lg-0">
+              <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
+              <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+            </form>
+          </div>
+        </div>
+
+      </div>
+    );
   }
 }
 
