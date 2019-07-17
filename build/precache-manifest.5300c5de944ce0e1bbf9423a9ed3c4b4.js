7f971dd22c5b43c2ec369542bc6278688c9b2e0b
fire chan <cken497@gmail.com>
2019-07-18 00:31:55 +0800

    fire deploy

---
+++ build/precache-manifest.5300c5de944ce0e1bbf9423a9ed3c4b4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "812441e46575952775b9a79047ae053f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4ce9443aa7836eaac56a",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "3b882fb5bc49452b0aab",
+    "url": "/static/css/main.24f75561.chunk.css"
+  },
+  {
+    "revision": "4ce9443aa7836eaac56a",
+    "url": "/static/js/2.18b9fc3d.chunk.js"
+  },
+  {
+    "revision": "3b882fb5bc49452b0aab",
+    "url": "/static/js/main.7f1034af.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "0212bfe2f998972808886df536ef1c13",
+    "url": "/static/media/bg.0212bfe2.png"
+  }
+]);
