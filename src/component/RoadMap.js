6519601caef5dd17b963b2dfbf68e710bceb4dcf
fire chan <cken497@gmail.com>
2019-07-31 23:48:04 +0800

    fire edit roadmap content and css

--- src/component/RoadMap.js
+++ src/component/RoadMap.js
@@ -1,36 +1,32 @@
 import React from 'react';
 import { StyleRoot } from 'radium'
 import '../css/roadMap.css';
-import roadmapItem from '../content/RoadMap.json';
+import { roadmapItem } from '../content/Roadmap.json';
 import Timeline from './helper/timeline'
 
 
 class RoadMap extends React.Component {
     render() {
-        // const timelineObj = JSON.parse(roadmapItem);
-
         return (
-            <StyleRoot>
-                <Timeline activeColor='yellow'>
-                    <div icon='S'><h1>Entry 1</h1></div>
-                    <div>
-                        <h1>Entry 2</h1>
-                        <ul>
-                            <li>arbitrary content in entries</li>
-                        </ul>
-                    </div>
-                    {[0, 1, 2, 3, 4].map(i =>
-                        <div key={i}>
-                            <h1>{i}</h1>
-                            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
-                            eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
-                            voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet
-                            clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit
-                            amet.
-                    </div>
-                    )}
-                </Timeline>
-            </StyleRoot>
+            <div className="road-map">
+                <div className="road-map-title">
+                    Our Road Map
+                </div>
+                <StyleRoot>
+                    <Timeline activeColor='yellow'>
+                        {roadmapItem.map((item, index) =>
+                            <div key={index}>
+                                <div className="road-map-item-title">{item.title}</div>
+                                <p>{item.milestoneDate}</p>
+                                <div className="road-map-desc">
+                                    {item.description}
+                                </div>
+                            </div>
+                        )}
+                    </Timeline>
+                </StyleRoot>
+            </div>
+
 
         );
     }
