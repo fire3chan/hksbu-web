0589e1889d10cb597b4def5556b4c769f2577d2e
fire chan <cken497@gmail.com>
2019-07-31 23:57:16 +0800

    fire build

--- build/precache-manifest.fc80fd1abf562d253ee2f1e12b86e345.js
+++ build/precache-manifest.fc80fd1abf562d253ee2f1e12b86e345.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "3b352316b66d4998f643368a25286e08",
+    "revision": "311a4e4e533ff80eef035e5178494f2f",
     "url": "/index.html"
   },
   {
-    "revision": "4ce9443aa7836eaac56a",
+    "revision": "1df8ab00c27994d173cc",
     "url": "/static/css/2.266e55a5.chunk.css"
   },
   {
-    "revision": "9ae01aa3e9bd046882f5",
-    "url": "/static/css/main.0feccfab.chunk.css"
+    "revision": "7cb889f8e2c3e026bb13",
+    "url": "/static/css/main.fc452584.chunk.css"
   },
   {
-    "revision": "4ce9443aa7836eaac56a",
-    "url": "/static/js/2.18b9fc3d.chunk.js"
+    "revision": "1df8ab00c27994d173cc",
+    "url": "/static/js/2.7833fd6e.chunk.js"
   },
   {
-    "revision": "9ae01aa3e9bd046882f5",
-    "url": "/static/js/main.2d1e599f.chunk.js"
+    "revision": "7cb889f8e2c3e026bb13",
+    "url": "/static/js/main.05bb197b.chunk.js"
   },
   {
     "revision": "42ac5946195a7306e2a5",
