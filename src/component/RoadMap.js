5b254ad297f2c4a35ecbe2314b5a9da76df0d2cd
fire chan <cken497@gmail.com>
2019-07-31 21:28:55 +0800

    fire change timeline style

--- src/component/RoadMap.js
+++ src/component/RoadMap.js
@@ -1,65 +1,36 @@
 import React from 'react';
+import { StyleRoot } from 'radium'
 import '../css/roadMap.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCircle } from '@fortawesome/free-solid-svg-icons'
+import roadmapItem from '../content/RoadMap.json';
+import Timeline from './helper/timeline'
+
 
 class RoadMap extends React.Component {
-
     render() {
+        // const timelineObj = JSON.parse(roadmapItem);
+
         return (
-            <div className="road-map">
-                <div className="road-map-title">Road Map</div>
-                <div className="road-map-desc">
-                    We are a company with vibrant past and the well thought-out plan to build the bright future.
-                </div>
-                <div className="time-line d-none d-md-block">
-                    <div id="content">
-                        <ul class="timeline">
-                            <li class="event" data-date="65Million B.C.">
-                                <h3>Dinosaurs Roamed the Earth</h3>
-                                <p>RAWWWWWWRRR </p>
-                            </li>
-                            <li class="event" data-date="2005">
-                                <h3>Creative Component Launched</h3>
-                                <p>"We can be all things to all people!" </p>
-                            </li>
-                            <li class="event" data-date="November 15, 2018">
-                                <h3>Squareflair Today</h3>
-
-                                <p>"We design and build from scratch!" </p> <p>When we say <em><strong>100% custom</strong></em> we mean it— and we build all sites on the Squarespace Developer platform.</p>
-                                <p>Did you know that all of our pixels are hand-forged from the rarest of subpixels grown and harvested in the <em>Nerd Forest</em>? <br /></p>
-
-                                <p><strong>Our success can be measured by lives and brands enhanced by 9+ years of 100% Squarespace-focused service!</strong></p>
-                            </li>
+            <StyleRoot>
+                <Timeline activeColor='yellow'>
+                    <div icon='S'><h1>Entry 1</h1></div>
+                    <div>
+                        <h1>Entry 2</h1>
+                        <ul>
+                            <li>arbitrary content in entries</li>
                         </ul>
                     </div>
-                </div>
-                <div className="time-line d-block d-md-none">
-                    <div id="mobile-content">
-                        <ul class="timeline">
-                            <li class="event" data-date="65Million B.C.">
-                                <h3>Dinosaurs Roamed the Earth</h3>
-                                <p>RAWWWWWWRRR </p>
-                            </li>
-                            <li class="event" data-date="2005">
-                                <h3>Creative Component Launched</h3>
-                                <p>"We can be all things to all people!" </p>
-                            </li>
-                            <li class="event" data-date="November 15, 2018">
-                                <h3>Squareflair Today</h3>
-
-                                <p>"We design and build from scratch!" </p> <p>When we say <em><strong>100% custom</strong></em> we mean it— and we build all sites on the Squarespace Developer platform.</p>
-                                <p>Did you know that all of our pixels are hand-forged from the rarest of subpixels grown and harvested in the <em>Nerd Forest</em>? <br /></p>
-
-                                <p><strong>Our success can be measured by lives and brands enhanced by 9+ years of 100% Squarespace-focused service!</strong></p>
-                            </li>
-                        </ul>
+                    {[0, 1, 2, 3, 4].map(i =>
+                        <div key={i}>
+                            <h1>{i}</h1>
+                            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
+                            eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
+                            voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet
+                            clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit
+                            amet.
                     </div>
-                </div>
-
-
-            </div>
-
+                    )}
+                </Timeline>
+            </StyleRoot>
 
         );
     }
